2ba9e8802b09c81688d42cc59cc51855a11f231e
Noah Buchanan <94912301+Buick142@users.noreply.github.com>
2024-04-19 14:55:19 +0200

    Made list

---
+++ todo.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const addTask = document.getElementById("addTask");
+    const addBtn = document.getElementById("addBtn");
+    const taskList = document.getElementById("taskList");
+})
+
+addBtn.addEventListener("click", function() {
+    const 
+})
